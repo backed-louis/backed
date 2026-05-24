9e5f18b772a6828cdad13625dbab6fffea597d4f
backed-louis <lemeelouis@gmail.com>
2026-05-24 10:38:45 +0100

    Update and rename next.config.js to next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'yt3.ggpht.com' },
+      { protocol: 'https', hostname: 'yt3.googleusercontent.com' },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "img-src 'self' data: https: *;",
+          },
+        ],
+      },
+    ]
+  },
+}
+
+export default nextConfig
